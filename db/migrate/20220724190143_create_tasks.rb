2aeb1b9869cf79ba1821c462587c72d591ef8687
Júlia Cordeiro <julia.c2001@hotmail.com>
2022-07-24 16:04:35 -0300

    Refactor tasks attributes

--- db/migrate/20220724190143_create_tasks.rb
+++ db/migrate/20220724190143_create_tasks.rb
@@ -1,7 +1,6 @@
 class CreateTasks < ActiveRecord::Migration[7.0]
   def change
     create_table :tasks do |t|
-      t.bigint :id
       t.string :description
       t.boolean :isUrgent
       t.boolean :isDone
