1de803b2d07326b7aa7e594e6f92c6bf2915e212
Júlia Cordeiro <julia.c2001@hotmail.com>
2022-07-24 17:02:06 -0300

    Implement get, put and delete requests

--- app/controllers/api/task_controller.rb
+++ app/controllers/api/task_controller.rb
@@ -9,7 +9,32 @@ class Api::TaskController < ApplicationController
     end
   end
 
+  def index
+    tasks = Task.order('created_at ASC')
+    render json: {status: 'SUCCESS', message:'Find all tasks', data:tasks},status: :ok
+  end
+
+  def update
+    task = Task.find(params[:id])
+
+    if task.update_attribute(:isDone, task_status)
+      render json: {status: 'SUCCESS', message:'Updated task', data:task},status: :ok
+    else
+      render json: {status: 'ERROR', message:'Task not updated', data:task.errors},status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    render json: {status: 'SUCCESS', message:'Deleted task', data:task},status: :ok
+  end
+
   private
+
+  def task_status
+    params.permit(:isDone)
+  end
 
   def task_params
     params.permit(:description, :isUrgent, :isDone)
